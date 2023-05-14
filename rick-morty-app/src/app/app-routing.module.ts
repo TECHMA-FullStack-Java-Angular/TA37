81f97c0ff15b3e9b7f5a154ba9bcc6073cc7e4f4
Paula Sousa <paulasousa137@gmail.com>
2023-05-14 19:32:14 +0200

    ud37 terminado

--- rick-morty-app/src/app/app-routing.module.ts
+++ rick-morty-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
+import { AboutComponent } from '../app/about/about.component';
+import { PersonajesComponent } from '../app/personajes/personajes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },       // Ruta raíz, página home
+  { path: 'about', component: AboutComponent }, // Ruta para la página About
+  { path: 'personajes', component: PersonajesComponent }, // Ruta para la página de Personajes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rick-morty-app/src/app/personajes/personajes.component.ts
+++ rick-morty-app/src/app/personajes/personajes.component.ts
@@ -7,18 +7,24 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./personajes.component.css']
 })
 export class PersonajesComponent implements OnInit {
+   //Declara una propiedad characters como un arreglo vacío ([]) de tipo any[]. 
+   //Esta propiedad se utilizará para almacenar los personajes obtenidos del archivo JSON.
   characters: any[] = [];
 
+  // Inyección de HttpClient en el constructor
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    // Llama al método para obtener los personajes al inicializar el componente
     this.fetchCharacters();
   }
 
   fetchCharacters() {
+     // Realiza una solicitud HTTP GET al archivo JSON
     this.http.get<any>('assets/database.json').subscribe(data => {
       this.characters = data.characters;
     });
   }
 }
+
 
